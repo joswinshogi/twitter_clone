ea7bc45680e9d4ba8dde3532601428f2c19d7dca
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2024-04-13 13:33:11 +0530

    first_config

---
+++ x-clone/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <div className=" w-full h-full flex justify-center items-center relative bg-black">
+      <div className=" max-w-screen-lg w-full h-full flex relative">
+        <section>left sidebar</section>
+        <div>Home timeline</div>
+        <section>Right section</section>
+      </div>
+    </div>
+  )
+}
+
+export default Home
